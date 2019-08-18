34de2a0c148bdbf75d91f767fade0bf04f8afa7e
rodrigofreund <rodrigofreund@gmail.com>
2019-08-18 11:04:35 -0300

    iniciando repositorio

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,19 @@
+class Negociacao {
+    constructor(data, quantidade, valor) {
+        this._data = data
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    obtemVolume() {
+        return this._quantidade * this._valor;
+    }
+
+    getData() {
+        return this._data;
+    }
+
+    getValor() {
+        return this._quantidade;
+    }
+}
