286a06a5dce556f43d2fa92d382707e1a06b84dc
Rodrigo Freund <rodrigofreund@gmail.com>
2019-08-28 00:24:26 -0300

    pagina 162

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,6 +1,6 @@
 class NegociacaoController {
     constructor() {
-        let $ = document.querySelector.bind(document);
+        const $ = document.querySelector.bind(document);
 
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
